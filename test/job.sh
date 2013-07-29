ea90be0acef2abdbdbedc1a0d03e2836c3d3c7a6
Matthias Wolf <matthias.wolf@cern.ch>
2013-07-29 14:25:28 -0400

    Add framework for grid-control job submission.
    See https://ekptrac.physik.uni-karlsruhe.de/trac/grid-control/wiki for
    information about grid-control.

---
+++ test/job.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+export VO_CMS_SW_DIR=/pscratch/osg/app/cmssoft/cms
+source $VO_CMS_SW_DIR/cmsset_default.sh
+export SCRAM_ARCH=slc5_amd64_gcc462
+
+cd /afs/crc.nd.edu/user/m/mwolf3/work/ttH/CMSSW_5_3_2_patch5
+cmsenv
+roaster $*
