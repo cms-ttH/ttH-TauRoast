56a191204cdfc7ae7832914deb2426c70e87f998
Matthias Wolf <matthias.wolf@cern.ch>
2014-01-23 15:02:06 -0500

    Fix running on the cluster.
    Adjust settings to the new current setup.  This is also taylored to TLL.

--- data/cluster/job.sh
+++ data/cluster/job.sh
@@ -6,6 +6,6 @@ export VO_CMS_SW_DIR=/pscratch/osg/app/cmssoft/cms
 source $VO_CMS_SW_DIR/cmsset_default.sh
 export SCRAM_ARCH=slc5_amd64_gcc462
 
-cd /afs/crc.nd.edu/user/m/mwolf3/work/ttH/CMSSW_5_3_2_patch5
+cd /afs/crc.nd.edu/user/m/mwolf3/work/ttH/releases/CMSSW_5_3_11_patch6
 cmsenv
 roaster -v $*
