359aab5dae68c38a1f5c65c4116c2f1104e47a71
Matthias Wolf <matthias@sushinara.net>
2015-04-14 05:40:07 -0400

    Update validation plotting.

--- test/plot_validate.py
+++ test/plot_validate.py
@@ -1,12 +1,14 @@
 import ROOT as r
 
 r.gROOT.SetBatch()
+r.gStyle.SetOptStat(0)
 
 class Plotter(object):
-    def __init__(self, infile, outfile):
+    def __init__(self, infile, oldfile, outfile):
         self.__outfile = outfile
 
         self.__f = r.TFile(infile)
+        self.__o = r.TFile(oldfile)
         self.__c = r.TCanvas()
         self.__first = True
 
@@ -20,15 +22,32 @@ class Plotter(object):
             opt2 = ")" if last and cut == "w < 0" else ""
 
             h = r.TH1F("I_am_a_histo", title + label, bins, xlow, xhigh)
+            i = r.TH1F("I_am_another_histo", title + label, bins, xlow, xhigh)
 
             if not any(isinstance(selection, cls) for cls in (list, tuple)):
                 selection = [selection]
 
             t = self.__f.Get("taus/" + tree)
+            u = self.__o.Get("taus/" + tree)
             for sel in selection:
                 t.Draw(sel + ">>+I_am_a_histo", cut)
+                u.Draw(sel + ">>+I_am_another_histo", cut)
 
-            h.Draw()
+            if h.Integral() > 0:
+                h.Scale(1. / h.Integral())
+            if i.Integral() > 0:
+                i.Scale(1. / i.Integral())
+
+            i.SetLineColor(r.kRed)
+
+            i.Draw()
+            h.Draw("same")
+
+            l = r.TLegend(.7, .7, .9, .9)
+            # l = r.TLegend()
+            l.AddEntry(h, "13 TeV")
+            l.AddEntry(i, "8 TeV")
+            l.Draw()
 
             self.__c.SetLogy()
             self.__c.SaveAs(self.__outfile + opt1)
@@ -39,18 +58,20 @@ class Plotter(object):
                 if h.GetBinContent(n) > 0.01 * mx:
                     break
             if n < h.GetNbinsX() * 0.66:
-                h.GetXaxis().SetRange(1, n + 2)
+                i.GetXaxis().SetRange(1, n + 2)
+
+            i.GetYaxis().SetRangeUser(0, 1.2 * max(i.GetMaximum(), h.GetMaximum()))
 
             self.__c.SaveAs(self.__outfile + opt2)
 
-p = Plotter("validate.root", "validate.pdf")
+p = Plotter("validate13.root", "validate8.root", "validate.pdf")
 p.plot("events", "njets", "ak5 genjets count", 80, 0, 80)
 p.plot("events", "njets30", "ak5 genjets count p_{T} > 30", 30, 0, 30)
 p.plot("events", ["tau1pt", "tau2pt"], "#tau p_{T} (from H)", 100, 0, 1500)
 p.plot("events", ["tau1eta", "tau2eta"], "#tau #eta (from H)", 100, -8, 8)
 p.plot("events", ["tau1decay", "tau2decay"], "#tau decay (0: hadronic)", 14, 0, 14)
-p.plot("events", ["top1pt", "top2pt"], "top p_{T} (status 62)", 100, 0, 2000)
-p.plot("events", ["top1eta", "top2eta"], "top #eta (status 62)", 100, -8, 8)
+p.plot("events", ["top1pt", "top2pt"], "top p_{T}", 100, 0, 2000)
+p.plot("events", ["top1eta", "top2eta"], "top #eta", 100, -8, 8)
 p.plot("events", "ditaumass", "Higgs mass from #tau_{1,2}", 40, 120, 130)
 p.plot("events", "ditaupt", "Higgs p_{T} from #tau_{1,2}", 100, 0, 2500)
 
