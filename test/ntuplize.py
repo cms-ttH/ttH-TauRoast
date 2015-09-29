900622c5e22a3e57d0614d1546ed68c58d9de430
Matthias Wolf <matthias@sushinara.net>
2015-09-29 09:21:47 -0400

    Update test files to run over.

--- test/ntuplize.py
+++ test/ntuplize.py
@@ -26,16 +26,7 @@ process.source = cms.Source("PoolSource",
             '/store/mc/RunIISpring15DR74/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/549DD972-2D03-E511-B843-0025905A48C0.root',
             '/store/mc/RunIISpring15DR74/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/648C0BFD-0D03-E511-A2E2-0025905B8598.root',
             '/store/mc/RunIISpring15DR74/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/8C0C8303-1A03-E511-8BE9-0025905A60A8.root',
-            '/store/mc/RunIISpring15DR74/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/8EE3EB01-2003-E511-9EDD-0025905A613C.root',
-            '/store/mc/RunIISpring15DR74/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/96039B4D-2503-E511-A57F-0025905B85AE.root',
-            '/store/mc/RunIISpring15DR74/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/9E83B57D-FD02-E511-B17D-001E67397C33.root',
-            '/store/mc/RunIISpring15DR74/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/A6F6DE52-0103-E511-B9FE-001E673971C5.root',
-            '/store/mc/RunIISpring15DR74/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/C0AD48FB-1D03-E511-A028-0025905B85A2.root',
-            '/store/mc/RunIISpring15DR74/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/CEF62DB0-3B03-E511-90EE-0025B3E063BA.root',
-            '/store/mc/RunIISpring15DR74/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/DADC2BD4-0403-E511-BD9E-002590200B3C.root',
-            '/store/mc/RunIISpring15DR74/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/E816F01A-0C03-E511-915B-002590200948.root',
-            '/store/mc/RunIISpring15DR74/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/E8A54186-1503-E511-9CB8-0025905B860C.root',
-            '/store/mc/RunIISpring15DR74/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/FCF18EA0-1603-E511-8B2B-002481E14E14.root'
+            '/store/mc/RunIISpring15DR74/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/8EE3EB01-2003-E511-9EDD-0025905A613C.root'
         ])
 )
 
