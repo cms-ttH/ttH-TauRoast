e881f87b67fe7f542411ab077db6d2777499b333
Matthias Wolf <matthias@sushinara.net>
2015-11-03 09:00:08 -0500

    Update crab validation config.

--- test/crab_validate.py
+++ test/crab_validate.py
@@ -9,9 +9,9 @@ config.JobType.pluginName  = 'Analysis'
 config.JobType.psetName    = 'validate.py'
 
 config.section_("Data")
-config.Data.inputDataset = '/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/RunIIWinter15GS-MCRUN2_71_V1-v1/GEN-SIM'
+config.Data.inputDataset = '/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2_ext3-v1/MINIAODSIM'
 config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = 25000
+config.Data.unitsPerJob = 100000
 config.Data.ignoreLocality = True
 
 config.section_("Site")
