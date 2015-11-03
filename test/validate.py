a6f143044bfa16fcfa587b330690e8c7c33e14a6
Matthias Wolf <matthias@sushinara.net>
2015-11-03 14:22:21 +0100

    Update validation pset.

--- test/validate.py
+++ test/validate.py
@@ -13,9 +13,9 @@ process.prefer("GlobalTag")
 
 readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring()
-process.source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
+process.source = cms.Source("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
-            '/store/mc/RunIISpring15DR74/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/00000/00D1004A-3B03-E511-8FE3-0025905A60B4.root',
+            '/store/mc/RunIISpring15MiniAODv2/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/50000/1278547D-C26D-E511-874F-842B2B185AAA.root',
 ] )
 
 process.TFileService = cms.Service("TFileService",
