144fd6fe70ba35f168c78ddbb902236307d899e6
Matthias Wolf <matthias@sushinara.net>
2015-11-10 07:50:18 -0500

    Fix validation file to run over.

--- test/validate.py
+++ test/validate.py
@@ -14,7 +14,7 @@ readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring()
 process.source = cms.Source("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
-            '/store/mc/RunIISpring15MiniAODv2/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/50000/787D9C97-C26D-E511-8A94-001E67E6F80F.root',
+            '/store/mc/RunIISpring15MiniAODv2/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/50000/1278547D-C26D-E511-874F-842B2B185AAA.root',
 ] )
 
 process.TFileService = cms.Service("TFileService",
