38d8f6751de8cc02005806649f2aa20a9d8032a8
Matthias Wolf <matthias@sushinara.net>
2015-11-12 04:28:36 -0500

    Switch validation to powheg samples.

--- test/validate.py
+++ test/validate.py
@@ -14,7 +14,11 @@ readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring()
 process.source = cms.Source("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
-            '/store/mc/RunIISpring15MiniAODv2/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/50000/1278547D-C26D-E511-874F-842B2B185AAA.root',
+    "/store/mc/RunIISpring15MiniAODv2/ttHToTT_M125_13TeV_powheg_pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/40000/02FC66B7-CF6D-E511-9B6C-20CF3027A59A.root",
+    "/store/mc/RunIISpring15MiniAODv2/ttHToTT_M125_13TeV_powheg_pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/40000/10000107-CF6D-E511-98E2-44A8423D7E31.root",
+    "/store/mc/RunIISpring15MiniAODv2/ttHToTT_M125_13TeV_powheg_pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/40000/10DAD8FF-CE6D-E511-9C21-0CC47A4DEDA2.root",
+    "/store/mc/RunIISpring15MiniAODv2/ttHToTT_M125_13TeV_powheg_pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/40000/18BB517A-CF6D-E511-9DF6-20CF3027A5D9.root",
+    "/store/mc/RunIISpring15MiniAODv2/ttHToTT_M125_13TeV_powheg_pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/40000/225296F8-CF6D-E511-9D65-001EC9EAF8B2.root"
 ] )
 
 process.TFileService = cms.Service("TFileService",
