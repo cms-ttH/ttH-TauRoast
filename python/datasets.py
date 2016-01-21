b0dfe3cb401d360c3ac190f2d7f3cb16023c0b5c
Matthias Wolf <matthias@sushinara.net>
2016-01-21 07:56:37 -0500

    Add a Lobster python config for v15.

---
+++ python/datasets.py
@@ -0,0 +1,48 @@
+datasets = [
+        # ("ttH2TT_125"                , "/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+        # ("ttH2TT_125_ext1"           , "/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2_ext1-v1/MINIAODSIM"),
+        # ("ttH2TT_125_ext2"           , "/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2_ext2-v1/MINIAODSIM"),
+        # ("ttH2TT_125_ext3"           , "/ttHJetToTT_M125_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2_ext3-v1/MINIAODSIM"),
+        # ("ttH2Nonbb_125"             , "/ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_pythia8_mWCutfix/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+        # ("ttH2Nonbb_125_ext1"        , "/ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_pythia8_mWCutfix/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2_ext1-v1/MINIAODSIM"),
+        # ("ttH2bb_125"                , "/ttHJetTobb_M125_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+        # ("ttH2bb_125_ext1"           , "/ttHJetTobb_M125_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2_ext1-v1/MINIAODSIM"),
+        # ("ttH2bb_125_ext2"           , "/ttHJetTobb_M125_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2_ext2-v1/MINIAODSIM"),
+        # ("ttH2bb_125_ext3"           , "/ttHJetTobb_M125_13TeV_amcatnloFXFX_madspin_pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2_ext3-v1/MINIAODSIM"),
+
+        ("ttH2TT_125_pow"            , "/ttHToTT_M125_13TeV_powheg_pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+        ("ttH2Nonbb_125_pow"         , "/ttHToNonbb_M125_13TeV_powheg_pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+        ("ttH2bb_125_pow"            , "/ttHTobb_M125_13TeV_powheg_pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+
+        # ("ttjets_amc"                , "/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v3/MINIAODSIM"),
+        # ("ttjets_mad"                , "/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+        # ("ttjets_dl_mad"             , "/TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+        # ("ttjets_dl_mad_ext"         , "/TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2_ext1-v1/MINIAODSIM"),
+        # ("ttjets_slt_mad"            , "/TTJets_SingleLeptFromT_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+        # ("ttjets_slt_mad_ext"        , "/TTJets_SingleLeptFromT_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2_ext1-v1/MINIAODSIM"),
+        # ("ttjets_sltbar_mad"         , "/TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+        # ("ttjets_sltbar_mad_ext"     , "/TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2_ext1-v1/MINIAODSIM"),
+
+        ("tt_pow"                    , "/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+        ("tt_pow_ext3"               , "/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2_ext3-v1/MINIAODSIM"),
+
+        ("st"                        , "/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v2/MINIAODSIM"),
+        ("stbar"                     , "/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+
+        ("dyjets_10_50"              , "/DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+        ("dyjets_50"                 , "/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+        ("wjets"                     , "/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+
+        ("zz"                        , "/ZZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+        ("wz"                        , "/WZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+        ("ww"                        , "/WW_TuneCUETP8M1_13TeV-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+
+        ("ttw"                       , "/TTWJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-madspin-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+        ("ttz"                       , "/TTZToLLNuNu_M-10_TuneCUETP8M1_13TeV-amcatnlo-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v2/MINIAODSIM"),
+        ("ttt_pow"                   , "/TTTo2L2Nu_13TeV-powheg/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM"),
+
+        ("data_SingleE_2015D_PR"     , "/SingleElectron/Run2015D-PromptReco-v4/MINIAOD"),
+        ("data_SingleE_2015D_Oct05"  , "/SingleElectron/Run2015D-05Oct2015-v1/MINIAOD"),
+        ("data_SingleMu_2015D_PR"    , "/SingleMuon/Run2015D-PromptReco-v4/MINIAOD"),
+        ("data_SingleMu_2015D_Oct05" , "/SingleMuon/Run2015D-05Oct2015-v1/MINIAOD")
+]
---
+++ test/ntuplize_lobster.py
@@ -0,0 +1,52 @@
+from lobster import cmssw
+from lobster.core import *
+
+from ttH.TauRoast.datasets import datasets
+
+version = "v15"
+
+storage = StorageConfiguration(
+        output=[
+            "hdfs:///store/user/matze/ttH/" + version,
+            "file:///hadoop/store/user/matze/ttH/" + version,
+            "root://ndcms.crc.nd.edu//store/user/matze/ttH/" + version,
+            "srm://T3_US_NotreDame/store/user/matze/ttH/" + version,
+            "chirp://earth.crc.nd.edu:9666/ttH/" + version
+        ]
+)
+
+processing = Category(
+        name='processing',
+        cores=1,
+        runtime=900,
+        memory=1500
+)
+
+workflows = []
+for label, path in datasets:
+    mask = None
+    params = []
+    if label.endswith('2015D_PR') or label.endswith('2015D_Oct05'):
+        mask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
+        params = ['data=True', 'globalTag=74X_dataRun2_v5']
+    workflows.append(Workflow(
+        label=label,
+        dataset=cmssw.Dataset(
+            dataset=path,
+            events_per_task=100000,
+            lumi_mask=mask
+        ),
+        category=processing,
+        cmssw_config='ntuplize.py',
+        arguments=params,
+        merge_size='3.5G'
+    ))
+
+config = Config(
+        label='tau',
+        workdir='/tmpscratch/users/matze/tau_' + version,
+        plotdir='/afs/crc.nd.edu/user/m/mwolf3/www/lobster/tau_' + version,
+        storage=storage,
+        workflows=workflows,
+        advanced=AdvancedOptions(log_level=1)
+)
