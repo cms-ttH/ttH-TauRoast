71dc208d20f62b5d5da4c0cf424547394b57dcb1
Matthias Wolf <matthias@sushinara.net>
2016-02-01 05:07:09 -0500

    Bring crab config up to speed.

--- test/crabby.py
+++ test/crabby.py
@@ -3,66 +3,37 @@ import re
 import yaml
 
 from WMCore.Configuration import Configuration
+from ttH.TauRoast.datasets import datasets
 
-def apply_matching(config):
-    if 'task defaults' not in config:
-        return config
-    defaults = config['task defaults']
-    matching = defaults.get('matching', [])
-    configs = []
-
-    for cfg in config['tasks']:
-        label = cfg['label']
-
-        for match in matching:
-            if re.search(match['label'], label):
-                for k, v in match.items():
-                    if k == 'label':
-                        continue
-                    if k not in cfg:
-                        cfg[k] = v
-        for k, v in defaults.items():
-            if k == 'matching':
-                continue
-            if k not in cfg:
-                cfg[k] = v
-
-        configs.append(cfg)
-
-    config['tasks'] = configs
-    del config['task defaults']
-
-    return config
-
-def dict2config(hash):
-    config = Configuration()
-
-    config.section_("General")
-    config.General.requestName = hash['label']
-    config.General.workArea = os.path.join(os.path.dirname(__file__), 'crabby_tau_v10')
-
-    config.section_("JobType")
-    config.JobType.pluginName  = 'Analysis'
-    config.JobType.psetName    = os.path.join(os.path.dirname(__file__), hash['cmssw config'])
-    if 'parameters' in hash:
-        config.JobType.pyCfgParams = hash['parameters']
-
-    config.section_("Data")
-    config.Data.inputDataset = hash['dataset']
-    config.Data.splitting = 'EventAwareLumiBased'
-    config.Data.unitsPerJob = 1000000
-    if 'lumi mask' in hash:
-        config.Data.lumiMask = hash['lumi mask']
-
-    config.section_("Site")
-    config.Site.storageSite = 'T2_CH_CERN'
-
-    return config
+version = "v17"
 
 def configs():
-    with open(os.path.join(os.path.dirname(__file__), 'lobster.yaml')) as f:
-        yml = yaml.load(f)
+    for label, path in datasets:
+        mask = None
+        params = []
+        if label.endswith('2015D_PR') or label.endswith('2015D_Oct05'):
+            mask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_v2.txt'
+            params = ['data=True', 'globalTag=74X_dataRun2_v5']
 
-    yml = apply_matching(yml)
-    for cfg in yml['tasks']:
-        yield dict2config(cfg)
+        config = Configuration()
+
+        config.section_("General")
+        config.General.requestName = label
+        config.General.workArea = os.path.join(os.path.dirname(__file__), 'crabby_tau_' + version)
+
+        config.section_("JobType")
+        config.JobType.pluginName  = 'Analysis'
+        config.JobType.psetName    = os.path.join(os.path.dirname(__file__), 'ntuplize.py')
+        config.JobType.pyCfgParams = params
+
+        config.section_("Data")
+        config.Data.inputDataset = path
+        config.Data.splitting = 'EventAwareLumiBased'
+        config.Data.unitsPerJob = 500000
+        if mask:
+            config.Data.lumiMask = mask
+
+        config.section_("Site")
+        config.Site.storageSite = 'T2_CH_CERN'
+
+        yield config
