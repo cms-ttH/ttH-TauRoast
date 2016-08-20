dc0589d54b5e45401fbc0ddd86268eb937fe8244
Matthias Wolf <matthias@sushinara.net>
2016-08-20 11:47:02 -0400

    Pep-8-ify.

--- data/plots/general.py
+++ data/plots/general.py
@@ -16,123 +16,125 @@ Leaf('npv', 'f', 'result = event.npv()')
 Leaf('nelectrons', 'f', 'int n = 0; for (const auto& l: leptons) n += l.electron(); result = n')
 Leaf('nmuons', 'f', 'int n = 0; for (const auto& l: leptons) n += l.muon(); result = n')
 Leaf('ntaus', 'i', 'result = len(taus)')
-Leaf('category', 'i', 'result = min(max(0, btags(jets) - 1), 1) * 3 + min(max(0, len(jets) - 2), 2)')
-Leaf('category_lj', 'i', 'result = min(max(0, btags(jets) - 2), 2) * 3 + min(max(0, len(jets) - 4), 2)')
+Leaf('category', 'i',
+     'result = min(max(0, btags(jets) - 1), 1) * 3 + min(max(0, len(jets) - 2), 2)')
+Leaf('category_lj', 'i',
+     'result = min(max(0, btags(jets) - 2), 2) * 3 + min(max(0, len(jets) - 4), 2)')
 
 Leaf('bdt', 'f', 'result = 0.', final=True)
 Plot(
-        name="general/BDT",
-        values=["bdt"],
-        labels=["BDT value", "Events"],
-        binning=[20, -1, 1]
+    name="general/BDT",
+    values=["bdt"],
+    labels=["BDT value", "Events"],
+    binning=[20, -1, 1]
 )
 
 Plot(
-        name="general/Events",
-        values=["events"],
-        labels=["", "Events"],
-        binning=[1, 0, 1]
+    name="general/Events",
+    values=["events"],
+    labels=["", "Events"],
+    binning=[1, 0, 1]
 )
 
 # Plot(
 #         name="general/Combos",
 #         values=["combos"],
-#         labels=["Number of passed #tau combinations", "Events"],
+# labels=["Number of passed #tau combinations", "Events"],
 #         binning=[5, 0, 5]
 # )
 Plot(
-        name="general/AllCombos",
-        values=['allcombos'],
-        labels=["Number of all #tau combinations", "Events"],
-        binning=[5, 0, 5]
+    name="general/AllCombos",
+    values=['allcombos'],
+    labels=["Number of all #tau combinations", "Events"],
+    binning=[5, 0, 5]
 )
 
 Plot(
-        name="general/NumInclusiveJets",
-        values=["njets+ntags"],
-        labels=["Number of inclusive jets", "Events"],
-        binning=[10, 0, 10]
+    name="general/NumInclusiveJets",
+    values=["njets+ntags"],
+    labels=["Number of inclusive jets", "Events"],
+    binning=[10, 0, 10]
 )
 Plot(
-        name="general/NumJets",
-        values=["njets"],
-        labels=["Number of jets", "Events"],
-        binning=[10, 0, 10]
+    name="general/NumJets",
+    values=["njets"],
+    labels=["Number of jets", "Events"],
+    binning=[10, 0, 10]
 )
 Plot(
-        name="general/NumTags",
-        values=["ntags"],
-        labels=["Number of tags", "Events"],
-        binning=[10, 0, 10]
-)
-
-Plot(
-        name="general/Category",
-        values=["category"],
-        labels=["", "Events"],
-        binning=[6, 0, 6],
-        binlabels=["2j1t", "3j1t", "4j1t", "2j2t", "3j2t", "4j2t"]
-)
-Plot(
-        name="general/Category_lj",
-        values=["category_lj"],
-        labels=["", "Events"],
-        log=True,
-        binning=[9, 0, 9],
-        binlabels=["4j2t", "5j2t", "6j2t", "4j3t", "5j3t", "6j3t", "4j4t", "5j4t", "6j4t"]
+    name="general/NumTags",
+    values=["ntags"],
+    labels=["Number of tags", "Events"],
+    binning=[10, 0, 10]
 )
 
 Plot(
-        name="general/NumVertices",
-        values=['npv'],
-        labels=["Number PV", "Events"],
-        binning=[40, 0, 40]
+    name="general/Category",
+    values=["category"],
+    labels=["", "Events"],
+    binning=[6, 0, 6],
+    binlabels=["2j1t", "3j1t", "4j1t", "2j2t", "3j2t", "4j2t"]
 )
 Plot(
-        name="general/NumVertices_coarse",
-        values=['npv'],
-        labels=["Number PV", "Events"],
-        binning=[20, 0, 40]
+    name="general/Category_lj",
+    values=["category_lj"],
+    labels=["", "Events"],
+    log=True,
+    binning=[9, 0, 9],
+    binlabels=["4j2t", "5j2t", "6j2t", "4j3t", "5j3t", "6j3t", "4j4t", "5j4t", "6j4t"]
+)
+
+Plot(
+    name="general/NumVertices",
+    values=['npv'],
+    labels=["Number PV", "Events"],
+    binning=[40, 0, 40]
 )
 Plot(
-        name="general/NumJetsOverview",
-        values=["ntags", "njets+ntags"],
-        labels=["Inclusive jets", "Tagged jets"],
-        binning=[12, 0, 12, 4, 0, 4]
+    name="general/NumVertices_coarse",
+    values=['npv'],
+    labels=["Number PV", "Events"],
+    binning=[20, 0, 40]
 )
 Plot(
-        name="general/NumLeptons",
-        values=["nelectrons + nmuons"],
-        labels=["Number of leptons", "Events"],
-        binning=[10, 0, 10]
+    name="general/NumJetsOverview",
+    values=["ntags", "njets+ntags"],
+    labels=["Inclusive jets", "Tagged jets"],
+    binning=[12, 0, 12, 4, 0, 4]
 )
 Plot(
-        name="general/NumElectrons",
-        values=["nelectrons"],
-        labels=["Number of e", "Events"],
-        binning=[3, 0, 3]
+    name="general/NumLeptons",
+    values=["nelectrons + nmuons"],
+    labels=["Number of leptons", "Events"],
+    binning=[10, 0, 10]
 )
 Plot(
-        name="general/NumMuons",
-        values=["nmuons"],
-        labels=["Number of #mu", "Events"],
-        binning=[3, 0, 3]
+    name="general/NumElectrons",
+    values=["nelectrons"],
+    labels=["Number of e", "Events"],
+    binning=[3, 0, 3]
+)
+Plot(
+    name="general/NumMuons",
+    values=["nmuons"],
+    labels=["Number of #mu", "Events"],
+    binning=[3, 0, 3]
 )
 
 Leaf('vtx_z', 'f', 'result = event.pv()[0].z()')
 Leaf('vtx_zerr', 'f', 'result = event.pv()[0].zError() * 1000')
 
 Plot(
-        name="vertex/ZCoord",
-        values=["vtx_z"],
-        labels=["Vertex Z coordinate", "Events"],
-        binning=[40, -20, 20]
+    name="vertex/ZCoord",
+    values=["vtx_z"],
+    labels=["Vertex Z coordinate", "Events"],
+    binning=[40, -20, 20]
 )
 Plot(
-        name="vertex/ZCoordError",
-        values=["vtx_zerr"],
-        labels=["Vertex Z coordinate Error (#mum)", "Events"],
-        binning=[40, 0, 2]
+    name="vertex/ZCoordError",
+    values=["vtx_zerr"],
+    labels=["Vertex Z coordinate Error (#mum)", "Events"],
+    binning=[40, 0, 2]
 )
 
 Leaf('met', 'f', 'result = met.Pt()')
@@ -158,34 +160,35 @@ result = ht.Pt()
 """)
 
 Plot(
-        name="general/MET",
-        values=["met"],
-        labels=["MET", "Events"],
-        binning=[20, 0, 300]
+    name="general/MET",
+    values=["met"],
+    labels=["MET", "Events"],
+    binning=[20, 0, 300]
 )
 Plot(
-        name="general/HT",
-        values=["ht"],
-        labels=["HT", "Events"],
-        binning=[20, 200, 1000]
+    name="general/HT",
+    values=["ht"],
+    labels=["HT", "Events"],
+    binning=[20, 200, 1000]
 )
 Plot(
-        name="general/MHT",
-        values=["mht"],
-        labels=["MHT", "Events"],
-        binning=[20, 0, 200]
+    name="general/MHT",
+    values=["mht"],
+    labels=["MHT", "Events"],
+    binning=[20, 0, 200]
 )
 
 ws = ["generator", "csvweight", "puweight"]
 ws += map(''.join, itertools.product(["jettaufake", "etaufake", "ideff"], ["up", "down"]))
-ws += map(''.join, itertools.product(["lfcont", "hfcont", "hfstats1", "hfstats2", "lfstats1", "lfstats2", "charmerr1", "charmerr2"], ["up", "down"]))
+ws += map(''.join, itertools.product(["lfcont", "hfcont", "hfstats1",
+                                      "hfstats2", "lfstats1", "lfstats2", "charmerr1", "charmerr2"], ["up", "down"]))
 for w in ws:
     Leaf('w_' + w, 'f', 'result = weights["{0}"]'.format(w))
 
 Plot(
-        name="weights/Generator",
-        values=["w_generator"],
-        weights=[],
-        labels=["Generator weight (unweighted)", "Events"],
-        binning=[20, -3, 5]
+    name="weights/Generator",
+    values=["w_generator"],
+    weights=[],
+    labels=["Generator weight (unweighted)", "Events"],
+    binning=[20, -3, 5]
 )
