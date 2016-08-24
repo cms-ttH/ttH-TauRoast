684392b0a43d14d5512777a2a250c04bf900f1e3
Matthias Wolf <matthias@sushinara.net>
2016-08-24 16:04:21 -0400

    Keep old CMSSW happy.

--- src/Fastlane.cc
+++ src/Fastlane.cc
@@ -164,10 +164,10 @@ fastlane::update_weights(std::unordered_map<std::string, double>& ws, const supe
    auto taus = combo.taus();
    int real_taus = std::count_if(std::begin(taus), std::end(taus),
          [](const superslim::Tau& t) { return t.match() == 5; });
-   int real_electrons = std::count_if(std::begin(taus), std::end(taus),
-         [](const superslim::Tau& t) { return t.match() == 1; });
-   int real_jets = std::count_if(std::begin(taus), std::end(taus),
-         [](const superslim::Tau& t) { return t.match() == 6; });
+   // int real_electrons = std::count_if(std::begin(taus), std::end(taus),
+   //       [](const superslim::Tau& t) { return t.match() == 1; });
+   // int real_jets = std::count_if(std::begin(taus), std::end(taus),
+   //       [](const superslim::Tau& t) { return t.match() == 6; });
 
    ws[lower("tauIdEffUp")] = std::pow(1 + tau_efficiency, real_taus);
    ws[lower("tauIdEffDown")] = std::pow(1 - tau_efficiency, real_taus);
