d66ed5aaac47ddaed95b75709dd24603ac1401f7
Matthias Wolf <matthias@sushinara.net>
2016-09-10 08:52:44 -0400

    Add some more lepton plots.

--- data/plots/lepton.py
+++ data/plots/lepton.py
@@ -36,28 +36,32 @@ for n in range(config.leptons):
 
     # Gen matching
     Plot(
-            name="leptons/generator/L{0}_Match".format(n + 1),
-            values=["lep{0}_match".format(n + 1)],
-            labels=[lbl + " id", "Events"],
-            binning=[6, 1, 7],
-            binlabels=["e", "#mu", "#tau #rightarrow e", "#tau #rightarrow #mu", "#tau #rightarrow #tau_{h}", "fake"]
+        name="leptons/generator/L{0}_Match".format(n + 1),
+        values=["lep{0}_match".format(n + 1)],
+        labels=[lbl + " id", "Events"],
+        binning=[6, 1, 7],
+        binlabels=["e", "#mu", "#tau #rightarrow e", "#tau #rightarrow #mu", "#tau #rightarrow #tau_{h}", "fake"]
     )
     Plot(
-            name="leptons/generator/L{0}_GenId".format(n + 1),
-            values=["lep{0}_genid".format(n + 1)],
-            labels=[lbl + " id", "Events"],
-            binning=[30, 0, 30]
+        name="leptons/generator/L{0}_GenId".format(n + 1),
+        values=["lep{0}_genid".format(n + 1)],
+        labels=[lbl + " id", "Events"],
+        binning=[30, 0, 30]
     )
     Plot(
-            name="leptons/generator/L{0}_ParentId".format(n + 1),
-            values=["lep{0}_genparentid".format(n + 1)],
-            labels=[lbl + "parent id", "Events"],
-            binning=[30, 0, 30]
+        name="leptons/generator/L{0}_ParentId".format(n + 1),
+        values=["lep{0}_genparentid".format(n + 1)],
+        labels=[lbl + "parent id", "Events"],
+        binning=[30, 0, 30]
     )
 
     Leaf('lep{0}_ip'.format(n + 1), 'f', 'result = abs(leptons[{0}].impactParameter())'.format(n))
     Leaf('lep{0}_dz'.format(n + 1), 'f', 'result = abs(leptons[{0}].dz())'.format(n))
     Leaf('lep{0}_reliso'.format(n + 1), 'f', 'result = abs(leptons[{0}].relativeIsolation())'.format(n))
+    Leaf('lep{0}_chargediso'.format(n + 1), 'f', 'result = abs(leptons[{0}].chargedIsolation())'.format(n))
+    Leaf('lep{0}_neutraliso'.format(n + 1), 'f', 'result = abs(leptons[{0}].neutralIsolation())'.format(n))
+    Leaf('lep{0}_mva'.format(n + 1), 'f', 'result = abs(leptons[{0}].mvaRaw())'.format(n))
+    Leaf('lep{0}_nontrig'.format(n + 1), 'f', 'result = abs(leptons[{0}].nonTriggeringId())'.format(n))
 
     # ID
     Plot(
@@ -76,5 +80,29 @@ for n in range(config.leptons):
         name="leptons/id/L{0}_RelIso".format(n + 1),
         values=["lep{0}_reliso".format(n + 1)],
         labels=[lbl + "RelIso", "Events"],
+        binning=[20, 0, 0.25]
+    )
+    Plot(
+        name="leptons/id/L{0}_ChargedIso".format(n + 1),
+        values=["lep{0}_chargediso".format(n + 1)],
+        labels=[lbl + "Charged Iso", "Events"],
+        binning=[20, 0, 0.25]
+    )
+    Plot(
+        name="leptons/id/L{0}_NeutralIso".format(n + 1),
+        values=["lep{0}_neutraliso".format(n + 1)],
+        labels=[lbl + "Neutral Iso", "Events"],
+        binning=[20, 0, 0.25]
+    )
+    Plot(
+        name="leptons/id/L{0}_MVA".format(n + 1),
+        values=["lep{0}_mva".format(n + 1)],
+        labels=[lbl + "MVA", "Events"],
+        binning=[20, 0, 0.25]
+    )
+    Plot(
+        name="leptons/id/L{0}_NonTrigID".format(n + 1),
+        values=["lep{0}_nontrig".format(n + 1)],
+        labels=[lbl + "Non Triggering ID", "Events"],
         binning=[20, 0, 0.25]
     )
