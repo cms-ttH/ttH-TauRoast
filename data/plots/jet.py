8461b0fa3b5ac7ceb9df5381e2ee11241b8a7300
Matthias Wolf <matthias@sushinara.net>
2016-09-15 04:06:31 -0400

    Adhere to the pep-8!

--- data/plots/jet.py
+++ data/plots/jet.py
@@ -23,16 +23,16 @@ from ttH.TauRoast.plotting import Plot
 # )
 
 pspace = [
-        ("", "jets"),
-        ("Untagged", "notags(jets)"),
-        ("Tagged", "tags(jets)")
+    ("", "jets"),
+    ("Untagged", "notags(jets)"),
+    ("Tagged", "tags(jets)")
 ]
 
 jspace = [
-        ("L", "Leading"),
-        ("SubL", "Sub-leading"),
-        ("SubSubL", "3rd"),
-        ("SubSubSubL", "4th")
+    ("L", "Leading"),
+    ("SubL", "Sub-leading"),
+    ("SubSubL", "3rd"),
+    ("SubSubSubL", "4th")
 ]
 
 return_list = """
@@ -47,40 +47,40 @@ for mode, fltr in pspace:
     Leaf('{0}jet_csv'.format(mode.lower()), '[f]', return_list.format(fltr, 'csv()'))
 
     Plot(
-            name="jets/kinematic/{0}J_Pt".format(mode),
-            values=['{0}jet_pt'.format(mode.lower())],
-            labels=["{0} jet P_{{T}}".format(mode), "Events"],
-            binning=[20, 30, 250]
+        name="jets/kinematic/{0}J_Pt".format(mode),
+        values=['{0}jet_pt'.format(mode.lower())],
+        labels=["{0} jet P_{{T}}".format(mode), "Events"],
+        binning=[20, 30, 250]
     )
     Plot(
-            name="jets/kinematic/{0}J_Eta".format(mode),
-            values=['{0}jet_eta'.format(mode.lower())],
-            labels=["{0} jet #eta".format(mode), "Events"],
-            binning=[40, -3, 3]
+        name="jets/kinematic/{0}J_Eta".format(mode),
+        values=['{0}jet_eta'.format(mode.lower())],
+        labels=["{0} jet #eta".format(mode), "Events"],
+        binning=[40, -3, 3]
     )
     Plot(
-            name="jets/id/{0}J_CSV".format(mode),
-            values=['{0}jet_csv'.format(mode.lower())],
-            labels=["{0} jet CSV".format(mode), "Events"],
-            binning=[30, 0, 1]
+        name="jets/id/{0}J_CSV".format(mode),
+        values=['{0}jet_csv'.format(mode.lower())],
+        labels=["{0} jet CSV".format(mode), "Events"],
+        binning=[30, 0, 1]
     )
 
     for n, (short, long) in enumerate(jspace):
         Plot(
-                name="jets/kinematic/{0}{1}J_Pt".format(mode, short),
-                values=['{0}jet_pt[{1}]'.format(mode.lower(), n)],
-                labels=["{0} {1} jet P_{{T}}".format(mode, long), "Events"],
-                binning=[20, 30, 250]
+            name="jets/kinematic/{0}{1}J_Pt".format(mode, short),
+            values=['{0}jet_pt[{1}]'.format(mode.lower(), n)],
+            labels=["{0} {1} jet P_{{T}}".format(mode, long), "Events"],
+            binning=[20, 30, 250]
         )
         Plot(
-                name="jets/kinematic/{0}{1}J_Eta".format(mode, short),
-                values=['{0}jet_eta[{1}]'.format(mode.lower(), n)],
-                labels=["{0} {1} jet #eta".format(mode, long), "Events"],
-                binning=[40, -3, 3]
+            name="jets/kinematic/{0}{1}J_Eta".format(mode, short),
+            values=['{0}jet_eta[{1}]'.format(mode.lower(), n)],
+            labels=["{0} {1} jet #eta".format(mode, long), "Events"],
+            binning=[40, -3, 3]
         )
         Plot(
-                name="jets/id/{0}{1}J_CSV".format(mode, short),
-                values=['{0}jet_csv[{1}]'.format(mode.lower(), n)],
-                labels=["{0} {1} jet CSV".format(mode, long), "Events"],
-                binning=[30, 0, 1]
+            name="jets/id/{0}{1}J_CSV".format(mode, short),
+            values=['{0}jet_csv[{1}]'.format(mode.lower(), n)],
+            labels=["{0} {1} jet CSV".format(mode, long), "Events"],
+            binning=[30, 0, 1]
         )
