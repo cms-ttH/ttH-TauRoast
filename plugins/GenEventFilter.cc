e01310a8e3718eecc6c55bf4e8c5b98f8ecf603a
Matthias Wolf <matthias@sushinara.net>
2016-09-21 06:51:19 -0400

    Add a gen-event filter modelling Christian's proposal.

---
+++ plugins/GenEventFilter.cc
@@ -0,0 +1,202 @@
+// -*- C++ -*-
+//
+// Package:    ttH/TauRoast
+// Class:      GenEventFilter
+// 
+/**\class TauRoast GenEventFilter.cc ttH/TauRoast/plugins/GenEventFilter.cc
+
+ Description: [one line class summary]
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Matthias Wolf
+//         Created:  Thu Sep 15 05:37:55 EDT 2016
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDFilter.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/JetReco/interface/GenJet.h"
+#include "DataFormats/JetReco/interface/GenJetCollection.h"
+#include "DataFormats/PatCandidates/interface/GenericParticle.h"
+
+//
+// class declaration
+//
+
+class GenEventFilter : public edm::EDFilter {
+   public:
+      explicit GenEventFilter(const edm::ParameterSet&);
+      ~GenEventFilter();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+   private:
+      virtual void beginJob() override;
+      virtual bool filter(edm::Event&, const edm::EventSetup&) override;
+      virtual void endJob() override;
+
+      // ----------member data ---------------------------
+      edm::EDGetTokenT<reco::GenParticleCollection> particle_token_;
+      edm::EDGetTokenT<reco::GenJetCollection> jet_token_;
+
+      std::vector<int> lepton_ids_;
+      std::vector<double> lepton_pt_;
+      std::vector<double> lepton_eta_;
+
+      double jet_pt_;
+      double jet_eta_;
+
+      double tau_pt_;
+      double tau_eta_;
+
+      int lepton_count_;
+      int jet_count_;
+      int tau_count_;
+      int total_count_;
+      int total_lepton_count_;
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+GenEventFilter::GenEventFilter(const edm::ParameterSet& config) :
+   lepton_ids_(config.getParameter<std::vector<int>>("leptonID")),
+   lepton_pt_(config.getParameter<std::vector<double>>("leptonPt")),
+   lepton_eta_(config.getParameter<std::vector<double>>("leptonEta")),
+   jet_pt_(config.getParameter<double>("jetPt")),
+   jet_eta_(config.getParameter<double>("jetEta")),
+   tau_pt_(config.getParameter<double>("tauPt")),
+   tau_eta_(config.getParameter<double>("tauEta")),
+   lepton_count_(config.getParameter<int>("minLeptons")),
+   jet_count_(config.getParameter<int>("minJets")),
+   tau_count_(config.getParameter<int>("minTaus")),
+   total_count_(config.getParameter<int>("minTotal")),
+   total_lepton_count_(config.getParameter<int>("minTotalLeptons"))
+{
+   particle_token_ = consumes<reco::GenParticleCollection>(config.getParameter<edm::InputTag>("genParticles"));
+   jet_token_ = consumes<reco::GenJetCollection>(config.getParameter<edm::InputTag>("genJets"));
+}
+
+
+GenEventFilter::~GenEventFilter()
+{
+}
+
+
+//
+// member functions
+//
+
+template<typename T> const T*
+mother(const T& p)
+{
+   for (unsigned int i = 0; i < p.numberOfMothers(); ++i) {
+      auto mother = dynamic_cast<const T*>(p.mother(i));
+
+      while (mother and mother->pdgId() == p.pdgId()) {
+         if (mother->numberOfMothers() > 0) {
+            mother = dynamic_cast<const T*>(mother->mother(0));
+         } else {
+            mother = 0;
+            break;
+         }
+      }
+
+      if (mother)
+         return mother;
+   }
+   return 0;
+}
+
+// ------------ method called on each new Event  ------------
+bool
+GenEventFilter::filter(edm::Event& event, const edm::EventSetup& setup)
+{
+   int leptons = 0;
+   int jets = 0;
+   int taus = 0;
+
+   edm::Handle<reco::GenParticleCollection> particles;
+   event.getByToken(particle_token_, particles);
+   edm::Handle<reco::GenJetCollection> genjets;
+   event.getByToken(jet_token_, genjets);
+
+   for (const auto& p: *particles) {
+      if (abs(p.pdgId()) == 15) {
+         bool leptonic = false;
+         for (unsigned int i = 0; i < p.numberOfDaughters(); ++i) {
+            auto d = p.daughter(i);
+            if (d and std::find(lepton_ids_.begin(), lepton_ids_.end(), abs(d->pdgId())) != lepton_ids_.end())
+               leptonic = true;
+         }
+         if ((not leptonic) and p.pt() > tau_pt_ and abs(p.eta()) < tau_eta_)
+            ++taus;
+      }
+      auto it = std::find(lepton_ids_.begin(), lepton_ids_.end(), abs(p.pdgId()));
+      auto idx = std::distance(lepton_ids_.begin(), it);
+      if (idx >= (int) lepton_ids_.size())
+         continue;
+
+      if (not (p.pt() > lepton_pt_[idx] and abs(p.eta()) < lepton_eta_[idx]))
+         continue;
+
+      // auto m = mother(p);
+      ++leptons;
+   }
+
+   jets = std::count_if(std::begin(*genjets), std::end(*genjets),
+         [&](const auto& j) { return j.pt() > jet_pt_ and abs(j.eta()) < jet_eta_; });
+
+   return (
+         leptons + jets + taus >= total_count_ and
+         leptons + taus >= total_lepton_count_ and
+         leptons >= lepton_count_ and
+         jets >= jet_count_ and
+         taus >= tau_count_
+   );
+}
+
+// ------------ method called once each job just before starting event loop  ------------
+void 
+GenEventFilter::beginJob()
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+GenEventFilter::endJob() {
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+GenEventFilter::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+//define this as a plug-in
+DEFINE_FWK_MODULE(GenEventFilter);
--- test/genfilter.py
+++ test/genfilter.py
@@ -33,16 +33,16 @@ process.source = cms.Source(
 process.njets = cms.EDFilter(
     "GenJetFilter",
     genTag=cms.InputTag('slimmedGenJets'),
-    nJets=cms.int32(4),
+    nJets=cms.int32(2),
     minPt=cms.double(20),
-    maxEta=cms.double(2.5)
+    maxEta=cms.double(2.7)
 )
 
 process.electron = cms.EDFilter(
     "GenLeptonFilter",
     genTag=cms.InputTag('prunedGenParticles'),
-    minPt=cms.double(30),
-    maxEta=cms.double(2.5),
+    minPt=cms.double(9),
+    maxEta=cms.double(2.7),
     pdgId=cms.int32(11),
     parentPdgId=cms.int32(24)
 )
@@ -50,29 +50,50 @@ process.electron = cms.EDFilter(
 process.muon = cms.EDFilter(
     "GenLeptonFilter",
     genTag=cms.InputTag('prunedGenParticles'),
-    minPt=cms.double(25),
-    maxEta=cms.double(2.5),
+    minPt=cms.double(9),
+    maxEta=cms.double(2.7),
     pdgId=cms.int32(13),
     parentPdgId=cms.int32(24)
 )
 
+process.eventf = cms.EDFilter(
+    "GenEventFilter",
+    genParticles=cms.InputTag('prunedGenParticles'),
+    genJets=cms.InputTag('slimmedGenJets'),
+    leptonID=cms.vint32(11, 13),
+    leptonPt=cms.vdouble(9, 9),
+    leptonEta=cms.vdouble(2.7, 2.7),
+    jetPt=cms.double(20),
+    jetEta=cms.double(2.7),
+    tauPt=cms.double(18),
+    tauEta=cms.double(2.7),
+    minLeptons=cms.int32(1),
+    minJets=cms.int32(2),
+    minTaus=cms.int32(0),
+    minTotal=cms.int32(7),
+    minTotalLeptons=cms.int32(1)
+)
+
 process.dump = cms.EDAnalyzer("EventContentAnalyzer")
 
-process.epath = cms.Path(
-    process.njets *
-    process.electron
-)
+process.evpath = cms.Path(process.eventf)
 
-process.mpath = cms.Path(
-    process.njets *
-    process.muon
-)
+# process.epath = cms.Path(
+#     process.njets *
+#     process.electron
+# )
+
+# process.mpath = cms.Path(
+#     process.njets *
+#     process.muon
+# )
 
 process.output = cms.OutputModule(
     "PoolOutputModule",
     fileName=cms.untracked.string(options.outputFile),
     outputCommands=cms.untracked.vstring(['keep *']),
-    SelectEvents=cms.untracked.PSet(SelectEvents=cms.vstring('epath', 'mpath')),
+    # SelectEvents=cms.untracked.PSet(SelectEvents=cms.vstring('epath', 'mpath')),
+    SelectEvents=cms.untracked.PSet(SelectEvents=cms.vstring('evpath')),
     dataset=cms.untracked.PSet(filterName=cms.untracked.string(''))
 )
 
