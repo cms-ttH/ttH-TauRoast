3841f0488f91a21df8c2e5feb9feb2237710fae0
Matthias Wolf <matthias@sushinara.net>
2016-09-21 05:02:45 -0400

    Provide more plots.

--- test/genfaketau.py
+++ test/genfaketau.py
@@ -26,13 +26,34 @@ taus_in = 'pt eta isoMVA03 genjet_pt genjet_eta genjetcharged_pt genjetcharged_e
 taus_in = ['tau_' + v for v in taus_in.split()]
 taus = read_root("test/genfaketau/out/ntuple.root", "ttjets", columns=taus_in, flatten=True)
 
+gen_in = 'chargedconstituents constituents chargedpt pt eta'
+gen_in = ['genjet_' + v for v in gen_in.split()]
+jets = read_root("test/genfaketau/out/ntuple.root", "ttjets", columns=gen_in, flatten=True)
+
 fakes = taus[(taus.tau_match == 6)]
 selection = taus[(taus.tau_match == 6) & (taus.tau_isoMVA03 >= 5) & (taus.tau_pt >= 20.)]
 
+labels = ['genjets', u'genjet fakes']
+nbins = range(0, 201, 10)
+
 fig = plt.figure()
-axs = fakes[[
-    'tau_pt', 'tau_genjet_pt', 'tau_genjetcharged_pt'
-]].plot.hist(subplots=True, layout=(3, 1), figsize=(11, 15), sharex=False, bins=30)
-for ax, (col, vals) in zip(axs.flat, selection[['tau_pt', 'tau_genjet_pt', 'tau_genjetcharged_pt']].iteritems()):
-    vals.hist(ax=ax, bins=30)
-plt.savefig('jetfakes_variables.png')
+n, bins, patches = plt.hist([jets.genjet_pt, selection.tau_genjet_pt], bins=nbins, normed=1)
+plt.legend(patches, labels)
+plt.savefig('jetfakes_genjet_pt.png')
+
+fig = plt.figure()
+n, bins, patches = plt.hist([jets.genjet_chargedpt, selection.tau_genjetcharged_pt], bins=nbins, normed=1)
+plt.legend(patches, labels)
+plt.savefig('jetfakes_genjet_chargedpt.png')
+
+nbins = range(0, 31, 1)
+
+fig = plt.figure()
+n, bins, patches = plt.hist([jets.genjet_constituents, selection.tau_genjet_constituents], bins=nbins, normed=1)
+plt.legend(patches, labels)
+plt.savefig('jetfakes_genjet_constituents.png')
+
+fig = plt.figure()
+n, bins, patches = plt.hist([jets.genjet_chargedconstituents, selection.tau_genjetcharged_constituents], bins=nbins, normed=1)
+plt.legend(patches, labels)
+plt.savefig('jetfakes_genjet_chargedconstituents.png')
