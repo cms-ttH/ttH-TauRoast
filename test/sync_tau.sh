2571c4624f3a3a50f69aa00a38dd00b9c2ceec0d
Matthias Wolf <matthias@sushinara.net>
2016-10-13 08:42:27 -0400

    Redirect ntuplizing output for sync.

--- test/sync_tau.sh
+++ test/sync_tau.sh
@@ -6,8 +6,8 @@ rm -f test/sync_tau/{ttH,ttH_skim,ttjets}/*.root
 syncdir=$HOME/www/ttH/sync/80X
 ttHfile=/store/mc/RunIISpring16MiniAODv2/ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_pythia8_mWCutfix/MINIAODSIM/PUSpring16RAWAODSIM_reHLT_80X_mcRun2_asymptotic_v14_ext1-v1/10000/00CD6E0F-003B-E611-9817-002590DE6E8A.root
 
-cmsRun test/ntuplize.py inputFiles=root://cms-xrd-global.cern.ch/$ttHfile takeAll=true outputFile=test/sync_tau/ttH/test.root reHLT=true &
-cmsRun test/ntuplize.py inputFiles=root://cms-xrd-global.cern.ch/$ttHfile outputFile=test/sync_tau/ttH_skim/test.root reHLT=true &
+cmsRun test/ntuplize.py inputFiles=root://cms-xrd-global.cern.ch/$ttHfile takeAll=true outputFile=test/sync_tau/ttH/test.root reHLT=true &> test/sync_tau/ttH.log &
+cmsRun test/ntuplize.py inputFiles=root://cms-xrd-global.cern.ch/$ttHfile outputFile=test/sync_tau/ttH_skim/test.root reHLT=true &> test/sync_tau/ttH_skim.log &
 # cmsRun test/ntuplize.py inputFiles=root://cms-xrd-global.cern.ch//store/mc/RunIIFall15MiniAODv2/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext3-v1/00000/00DF0A73-17C2-E511-B086-E41D2D08DE30.root takeAll=true outputFile=test/sync_tau/ttjets/test.root &
 # cmsRun test/ntuplize.py inputFiles=file:0C6DA13E-38C8-E511-8F6E-00259055220A.root takeAll=true outputFile=test/sync_tau/ttH/test.root channels=tll maxEvents=-1 &
 # cmsRun test/ntuplize.py inputFiles=file:00DF0A73-17C2-E511-B086-E41D2D08DE30.root takeAll=true outputFile=test/sync_tau/ttjets/test.root channels=tll maxEvents=-1 &
