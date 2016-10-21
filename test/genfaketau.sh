a55203075dcf9c125449a3a87358eb03790a024a
Matthias Wolf <matthias@sushinara.net>
2016-10-21 10:58:16 -0400

    Increase stats for fake τ study.

--- test/genfaketau.sh
+++ test/genfaketau.sh
@@ -1,10 +1,8 @@
 set -e
 
-MAXEVENTS=40000
+MAXEVENTS=400000
 
 mkdir -p test/genfaketau/ttjets
 rm -f test/genfaketau/ttjets/*.root
 
-cmsRun test/ntuplize.py inputFiles_load=test/genfilter.files outputFile=test/genfaketau/ttjets/test.root channels=ttl maxEvents=$MAXEVENTS takeAll=true saveGenInfo=true &
-
-wait
+cmsRun test/ntuplize.py inputFiles_load=test/genfilter.files outputFile=test/genfaketau/ttjets/test.root channels=ttl maxEvents=$MAXEVENTS takeAll=true saveGenInfo=true
