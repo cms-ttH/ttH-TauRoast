e68f644e0df79a94b2dbfa5dc39cca5a50eb4731
Matthias Wolf <matthias@sushinara.net>
2016-10-28 08:36:38 -0400

    Provide customization for gen filtering.

---
+++ python/customGenFilter.py
@@ -0,0 +1,26 @@
+import FWCore.ParameterSet.Config as cms
+
+
+def customizeForGenFiltering(process):
+    process.ttHGenFilter = cms.EDFilter(
+        "GenEventFilter",
+        genParticles=cms.InputTag('prunedGenParticles'),
+        genJets=cms.InputTag('slimmedGenJets'),
+        leptonID=cms.vint32(11, 13),
+        leptonPt=cms.vdouble(25, 20),
+        leptonEta=cms.vdouble(2.7, 2.7),
+        jetPt=cms.double(20),
+        jetEta=cms.double(2.7),
+        tauPt=cms.double(18),
+        tauEta=cms.double(2.7),
+        useFakes=cms.bool(False),
+        fakeCut=cms.double(-0.155),
+        minLeptons=cms.int32(1),
+        minJets=cms.int32(3),
+        minTaus=cms.int32(0),
+        minTotal=cms.int32(6),
+        minTotalLeptons=cms.int32(1)
+    )
+    if hasattr(process, 'generation_step'):
+        process.generation_step *= process.ttHGenFilter
+    return process
