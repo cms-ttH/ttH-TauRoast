8e621a0ccc80dca10e688f8c5d5b6c6d4e0100dc
Matthias Wolf <matthias@sushinara.net>
2016-11-08 07:31:03 -0500

    Adjust genfilter test setup.

--- test/genfilter.py
+++ test/genfilter.py
@@ -37,64 +37,16 @@ process.source = cms.Source(
 if len(options.events) > 0:
     process.source.eventsToProcess = cms.untracked.VEventRange(options.events)
 
-process.njets = cms.EDFilter(
-    "GenJetFilter",
-    genTag=cms.InputTag('slimmedGenJets'),
-    nJets=cms.int32(2),
-    minPt=cms.double(20),
-    maxEta=cms.double(2.7)
-)
-
-process.electron = cms.EDFilter(
-    "GenLeptonFilter",
-    genTag=cms.InputTag('prunedGenParticles'),
-    minPt=cms.double(9),
-    maxEta=cms.double(2.7),
-    pdgId=cms.int32(11),
-    parentPdgId=cms.int32(24)
-)
-
-process.muon = cms.EDFilter(
-    "GenLeptonFilter",
-    genTag=cms.InputTag('prunedGenParticles'),
-    minPt=cms.double(9),
-    maxEta=cms.double(2.7),
-    pdgId=cms.int32(13),
-    parentPdgId=cms.int32(24)
-)
-
-process.eventf = cms.EDFilter(
-    "GenEventFilter",
-    genParticles=cms.InputTag('prunedGenParticles'),
-    genJets=cms.InputTag('slimmedGenJets'),
-    leptonID=cms.vint32(11, 13),
-    leptonPt=cms.vdouble(25, 20),
-    leptonEta=cms.vdouble(2.7, 2.7),
-    jetPt=cms.double(20),
-    jetEta=cms.double(2.7),
-    tauPt=cms.double(18),
-    tauEta=cms.double(2.7),
-    fakeCut=cms.double(-0.155),
-    minLeptons=cms.int32(1),
-    minJets=cms.int32(3),
-    minTaus=cms.int32(0),
-    minTotal=cms.int32(6),
-    minTotalLeptons=cms.int32(1)
-)
+process.load('ttH.TauMCGeneration.eventFilter_cfi')
+process.ttHGenFilter.useFakeTaus = cms.bool(True)
+process.ttHGenFilter.minTaus = cms.int32(2)
+process.ttHGenFilter.fakeCut = cms.double(0.)
+process.ttHGenFilter.genParticles = cms.InputTag('prunedGenParticles')
+process.ttHGenFilter.genJets = cms.InputTag('slimmedGenJets')
 
 process.dump = cms.EDAnalyzer("EventContentAnalyzer")
 
-process.evpath = cms.Path(process.eventf)
-
-# process.epath = cms.Path(
-#     process.njets *
-#     process.electron
-# )
-
-# process.mpath = cms.Path(
-#     process.njets *
-#     process.muon
-# )
+process.evpath = cms.Path(process.ttHGenFilter)
 
 process.output = cms.OutputModule(
     "PoolOutputModule",
