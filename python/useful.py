35247d9f5a0cbe9e65bc446274d96735554cb8e8
Matthias Wolf <matthias@sushinara.net>
2016-12-19 12:34:34 -0500

    Add another c++ header for leaves.

--- python/useful.py
+++ python/useful.py
@@ -159,6 +159,7 @@ def code2cut(name, code):
 def code2leaf(name, typename, code):
     stub = hashlib.sha1(name + code).hexdigest()[:7]
     chunck = """
+        #include <algorithm>
         #include "ttH/TauRoast/interface/Fastlane.h"
 
         void fct_{f}(const superslim::Event& event,
