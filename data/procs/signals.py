ed1d80f8f3a125e609b331d0886641610c56a9c5
Matthias Wolf <matthias@sushinara.net>
2017-01-27 05:56:16 -0500

    Use amc@NLO for limits.

--- data/procs/signals.py
+++ data/procs/signals.py
@@ -1,15 +1,15 @@
 from ttH.TauRoast.processing import BasicProcess, CombinedProcess
 
-BasicProcess(
-    name="ttH2TT_125_amc",
-    fullname="t#bar{t}H(125) #rightarrow #tau_{h} #tau_{h}",
-    limitname="ttHTT125_amc",
-    paths=['ttH2TT_125', 'ttH2TT_125_ext1', 'ttH2TT_125_ext2', 'ttH2TT_125_ext3'],
-    cross_section=0.5085 * 6.32e-2,
-    events=8378353
-    # split: {hbb: 0, hcc: 1, hww: 2, hzz: 3, htt: 4, hgg: 5, hgluglu: 6, hzg: 7}
-    # split: {htt: 4}
-)
+# BasicProcess(
+#     name="ttH2TT_125_amc",
+#     fullname="t#bar{t}H(125) #rightarrow #tau_{h} #tau_{h}",
+#     limitname="ttHTT125_amc",
+#     paths=['ttH2TT_125', 'ttH2TT_125_ext1', 'ttH2TT_125_ext2', 'ttH2TT_125_ext3'],
+#     cross_section=0.5085 * 6.32e-2,
+#     events=8378353
+#     # split: {hbb: 0, hcc: 1, hww: 2, hzz: 3, htt: 4, hgg: 5, hgluglu: 6, hzg: 7}
+#     # split: {htt: 4}
+# )
 
 BasicProcess(
     name="ttH2TT_125_real_amc",
@@ -40,6 +40,45 @@ BasicProcess(
     cross_section=0.2586,  # 0.5085 * (1 - 0.577),
     events=9992683,
     cutflow='signal'
+)
+
+BasicProcess(
+    name="ttH2TT_125_amc",
+    fullname="t#bar{t}H(125) #rightarrow b#bar{b}",
+    limitname="ttH_htt",
+    paths=['ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_pythia8_mWCutfix_ext1_v1'],
+    cross_section=0.2586,  # 0.5085 * (1 - 0.577),
+    events=9992683,
+    cutflow='signal',
+    additional_cuts=[
+        ('process', 'abs(event.higgsDecay()) == 15')
+    ]
+)
+
+BasicProcess(
+    name="ttH2WW_125_amc",
+    fullname="t#bar{t}H(125) #rightarrow b#bar{b}",
+    limitname="ttH_hww",
+    paths=['ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_pythia8_mWCutfix_ext1_v1'],
+    cross_section=0.2586,  # 0.5085 * (1 - 0.577),
+    events=9992683,
+    cutflow='signal',
+    additional_cuts=[
+        ('process', 'abs(event.higgsDecay()) == 24')
+    ]
+)
+
+BasicProcess(
+    name="ttH2ZZ_125_amc",
+    fullname="t#bar{t}H(125) #rightarrow b#bar{b}",
+    limitname="ttH_hzz",
+    paths=['ttHJetToNonbb_M125_13TeV_amcatnloFXFX_madspin_pythia8_mWCutfix_ext1_v1'],
+    cross_section=0.2586,  # 0.5085 * (1 - 0.577),
+    events=9992683,
+    cutflow='signal',
+    additional_cuts=[
+        ('process', 'abs(event.higgsDecay()) == 23')
+    ]
 )
 
 BasicProcess(
