473b69f10fde167eecb9fb279ce1081a7d4c222f
Matthias Wolf <matthias@sushinara.net>
2017-03-10 10:33:47 -0500

    Revert legend scale factor.

--- python/plotting.py
+++ python/plotting.py
@@ -492,7 +492,7 @@ class Plot(object):
             base_histo.GetYaxis().SetTitle("")
 
         if config.get("legend", True):
-            scale = 1.175 + 0.03 * (
+            scale = 1.175 + 0.05 * (
                 math.ceil(len(config['backgrounds'] + config.get('data', [])) / 4. + 1)
                 + len(config['signals']))
 
