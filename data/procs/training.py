d60aeb6f937d0a721f9608e215e30323004baeeb
Matthias Wolf <matthias@sushinara.net>
2017-04-08 15:16:45 -0400

    Update training files.

--- data/procs/training.py
+++ data/procs/training.py
@@ -44,9 +44,44 @@ BasicProcess(
 
 CombinedProcess(
     name="ttjets_pow_train",
-    fullname="t#bar{t}",
+    fullname="t#bar{t} (fakes)",
     limitname="ttjets",
     subprocesses=["ttjets_sl_pow_train", "ttjets_dl_pow_train"]
+)
+
+BasicProcess(
+    name="ttjets_sl_pow_nominal",
+    fullname="t #bar{t} SL",
+    paths=[
+        "TTToSemilepton_TuneCUETP8M2_ttHtranche3_13TeV_powheg_pythia8_faster_v8_p1_v1",
+        "TTToSemilepton_TuneCUETP8M2_ttHtranche3_13TeV_powheg_pythia8_faster_v8_p2_v1",
+        "TTToSemilepton_TuneCUETP8M2_ttHtranche3_13TeV_powheg_pythia8_faster_v8_p3_v1",
+    ],
+    # cross_section=831.76 * wleptonic * (1 - wleptonic),
+    cross_section=182 * 2,
+    events=500 * 1000 * 1000,
+    cutflow='nominal'
+)
+
+BasicProcess(
+    name="ttjets_dl_pow_nominal",
+    fullname="t #bar{t} FL",
+    paths=[
+        "TTTo2L2Nu_TuneCUETP8M2_ttHtranche3_13TeV_powheg_pythia8_faster_v8_p1_v1",
+        "TTTo2L2Nu_TuneCUETP8M2_ttHtranche3_13TeV_powheg_pythia8_faster_v8_p2_v1",
+        "TTTo2L2Nu_TuneCUETP8M2_ttHtranche3_13TeV_powheg_pythia8_faster_v8_p3_v1",
+    ],
+    # cross_section=831.76 * wleptonic ** 2,
+    cross_section=87.3,
+    events=250 * 1000 * 1000,
+    cutflow='nominal'
+)
+
+CombinedProcess(
+    name="ttjets_pow_nominal",
+    fullname="t#bar{t} (nominal)",
+    limitname="ttjets",
+    subprocesses=["ttjets_sl_pow_nominal", "ttjets_dl_pow_nominal"]
 )
 
 BasicProcess(
