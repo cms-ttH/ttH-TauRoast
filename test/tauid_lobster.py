f12c029d23862e6a0c9b086dadb34c236300f7ed
Matthias Wolf <matthias@sushinara.net>
2017-09-26 13:35:33 -0400

    Start tau id study.

---
+++ test/tauid_lobster.py
@@ -0,0 +1,115 @@
+from lobster import cmssw
+from lobster.core import AdvancedOptions, Category, Config, StorageConfiguration, Workflow
+# from lobster.monitor.elk import ElkInterface
+
+from ttH.TauRoast.datasets import datasets, mctag
+
+version = "tauid_v1"
+tag = "all"
+
+globaltag_mc = "80X_mcRun2_asymptotic_2016_TrancheIV_v8"
+globaltag_data = "80X_dataRun2_2016SeptRepro_v7"
+globaltag_data_h = "80X_dataRun2_Prompt_v16"
+
+lumimask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
+
+storage = StorageConfiguration(
+    output=[
+        "hdfs://eddie.crc.nd.edu:19000/store/user/matze/ttH/{}".format(version),
+        "file:///hadoop/store/user/matze/ttH/{}".format(version),
+        "root://deepthought.crc.nd.edu//store/user/matze/ttH/{}".format(version),
+        # "chirp://eddie.crc.nd.edu:9094/store/user/matze/ttH/{}".format(version),
+        "gsiftp://T3_US_NotreDame/store/user/matze/ttH/{}".format(version),
+        "srm://T3_US_NotreDame/store/user/matze/ttH/{}".format(version)
+    ]
+)
+
+data = Category(
+    name='data',
+    cores=2,
+    runtime=90 * 60,
+    disk=400,
+    memory=2000
+)
+
+tth = Category(
+    name='ttH',
+    cores=2,
+    runtime=90 * 60,
+    disk=400,
+    memory=2000
+)
+
+mc = Category(
+    name='mc',
+    cores=2,
+    runtime=90 * 60,
+    disk=400,
+    memory=2000
+)
+
+workflows = []
+for path in datasets(tag):
+    _, major, minor, _ = path.split('/')
+    minor = mctag(minor)
+    label = (major + '_' + minor).replace('-', '_')
+    mask = None
+    params = ['globalTag=' + globaltag_mc, 'channels=ttl', 'takeAll=true']
+    category = mc
+
+    if not (label.startswith('ttHJetToN') or label.startswith('TTTo')):
+        continue
+
+    if '/Run2016' in path and path.endswith('MINIAOD'):
+        if '/Run2016H' in path:
+            gt = globaltag_data_h
+        else:
+            gt = globaltag_data
+        mask = lumimask
+        params = ['data=True', 'globalTag=' + gt, 'channels=ttl']
+        category = data
+        if '/Run2016G' in path or '/Run2017H' in path:
+            params += ['hip=false']
+    elif label.startswith('ttH'):
+        category = tth
+        params += ['saveGenInfo=true']
+
+    if 'amcatnlo' in path:
+        if label.startswith('ttH'):
+            params += ['sample=ttH']
+        if label.startswith('TTW'):
+            params += ['sample=ttW']
+        if label.startswith('TTZ'):
+            params += ['sample=ttZ']
+
+    workflows.append(Workflow(
+        label=label,
+        dataset=cmssw.Dataset(
+            dataset=path,
+            events_per_task=150000,
+            lumi_mask=mask,
+            dbs_instance='phys03'
+        ),
+        category=category,
+        merge_size='3g',
+        pset='tauid_ntuplize.py',
+        arguments=params
+    ))
+
+config = Config(
+    label='tau_{}_{}'.format(version, tag),
+    workdir='/tmpscratch/users/matze/ttH/{}_{}'.format(version, tag),
+    plotdir='/afs/crc.nd.edu/user/m/mwolf3/www/lobster/ttH/{}_{}'.format(version, tag),
+    # elk=ElkInterface('elk.crc.nd.edu', 9200, 'elk.crc.nd.edu', 5601, 'ttH_{}_{}'.format(version, tag).lower()),
+    storage=storage,
+    workflows=workflows,
+    advanced=AdvancedOptions(
+        bad_exit_codes=[127, 169],
+        log_level=1,
+        xrootd_servers=[
+            # 'deepthought.crc.nd.edu'
+            'ndcms.crc.nd.edu',
+            # 'cmsxrootd.fnal.gov'
+        ]
+    )
+)
---
+++ test/tauid_ntuplize.py
@@ -0,0 +1,249 @@
+import os.path
+
+import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+from FWCore.ParameterSet.Utilities import convertToUnscheduled
+
+from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import updatedPatJets
+from PhysicsTools.PatAlgos.producersLayer1.jetUpdater_cff import updatedPatJetCorrFactors
+from PhysicsTools.PatUtils.tools.runMETCorrectionsAndUncertainties import runMetCorAndUncFromMiniAOD
+
+
+def add_channel(channel, path):
+    dirname, filename = os.path.split(path)
+    return os.path.join(dirname, "{}_{}".format(channel, filename))
+
+
+options = VarParsing.VarParsing('analysis')
+options.outputFile = "test.root"
+options.register("channels", "",
+                 VarParsing.VarParsing.multiplicity.list,
+                 VarParsing.VarParsing.varType.string,
+                 "Which channel(s) to run")
+options.register("events", "",
+                 VarParsing.VarParsing.multiplicity.list,
+                 VarParsing.VarParsing.varType.string,
+                 "Selected events to run over")
+options.register("takeVLoose", False,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.bool,
+                 "Take very loose taus")
+options.register("takeAll", False,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.bool,
+                 "Save as many events as possible")
+options.register("dump", False,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.bool,
+                 "Dump event content and quit after one event.")
+options.register("sample", "",
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 "Sample name to use")
+options.register("data", False,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.bool,
+                 "Indicate if data is being used (or MC)")
+options.register("saveGenInfo", False,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.bool,
+                 "Indicate if gen collections should be saved")
+options.register("hlt", "HLT",
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 "Indicate the HLT result label")
+options.register("hip", True,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.bool,
+                 "Indicate if HIP safe mu ID is to be used.")
+options.register("globalTag", "80X_mcRun2_asymptotic_2016_TrancheIV_v8",
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 "Global tag to use")
+options.setDefault("channels", "ttl")
+options.parseArguments()
+
+process = cms.Process("Taus")
+
+process.options = cms.untracked.PSet(
+    wantSummary=cms.untracked.bool(True),
+    numberOfThreads=cms.untracked.uint32(1),
+    numberOfStreams=cms.untracked.uint32(0),
+    SkipEvent=cms.untracked.vstring('ProductNotFound')
+)
+
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 2000
+
+process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(options.maxEvents))
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
+process.GlobalTag.globaltag = options.globalTag
+
+process.source = cms.Source(
+    "PoolSource",
+    fileNames=cms.untracked.vstring(options.inputFiles),
+    # eventsToProcess=cms.untracked.VEventRange('1:1952714')
+)
+
+if len(options.events) > 0:
+    process.source.eventsToProcess = cms.untracked.VEventRange(options.events)
+
+# Jets
+# ----
+
+process.patJetCorrFactorsReapplyJEC = updatedPatJetCorrFactors.clone(
+    src=cms.InputTag("slimmedJets"),
+    levels=['L1FastJet', 'L2Relative', 'L3Absolute'],
+    payload='AK4PFchs'  # Make sure to choose the appropriate levels and payload here!
+)
+if options.data:
+    process.patJetCorrFactorsReapplyJEC.levels.append('L2L3Residual')
+
+process.patJetsReapplyJEC = updatedPatJets.clone(
+    jetSource=cms.InputTag("slimmedJets"),
+    jetCorrFactorsSource=cms.VInputTag(cms.InputTag("patJetCorrFactorsReapplyJEC"))
+)
+
+process.jetPath = cms.Path(
+    process.patJetCorrFactorsReapplyJEC *
+    process.patJetsReapplyJEC
+)
+
+# If you only want to re-correct and get the proper uncertainties
+runMetCorAndUncFromMiniAOD(process,
+                           isData=options.data)
+
+# If you would like to re-cluster and get the proper uncertainties
+# runMetCorAndUncFromMiniAOD(process,
+#                        isData=True (or False),
+#                        pfCandColl=cms.InputTag("packedPFCandidates"),
+#                        recoMetFromPFCs=True,
+#                        )
+
+# Bad Muons
+# ---------
+
+process.badGlobalMuonTagger = cms.EDFilter(
+    "BadGlobalMuonTagger",
+    muons=cms.InputTag("slimmedMuons"),
+    vtx=cms.InputTag("offlineSlimmedPrimaryVertices"),
+    muonPtCut=cms.double(20),
+    selectClones=cms.bool(False),
+    taggingMode=cms.bool(True),
+    verbose=cms.untracked.bool(False)
+)
+process.cloneGlobalMuonTagger = process.badGlobalMuonTagger.clone(
+    selectClones=cms.bool(True)
+)
+
+process.removeBadAndCloneGlobalMuons = cms.EDProducer(
+    "MuonRefPruner",
+    input=cms.InputTag("slimmedMuons"),
+    toremove=cms.InputTag("badGlobalMuonTagger", "bad"),
+    toremove2=cms.InputTag("cloneGlobalMuonTagger", "bad")
+)
+
+process.noBadGlobalMuons = cms.Sequence(
+    process.cloneGlobalMuonTagger +
+    process.badGlobalMuonTagger +
+    process.removeBadAndCloneGlobalMuons
+)  # in tagging mode, these modules return always "true"
+
+
+process.load("RecoEgamma.ElectronIdentification.ElectronMVAValueMapProducer_cfi")
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+process.load("ttH.LeptonID.ttHLeptons_cfi")
+
+process.ttHLeptons.LooseCSVWP = cms.double(0.5426)
+process.ttHLeptons.MediumCSVWP = cms.double(0.8484)
+process.ttHLeptons.IsHIPSafe = cms.bool(options.hip)
+
+process.lepPath = cms.Path(
+    process.noBadGlobalMuons *
+    process.fullPatMetSequence *
+    process.electronMVAValueMapProducer *
+    process.ttHLeptons
+)
+
+process.ttHLeptons.rhoParam = "fixedGridRhoFastjetCentralNeutral"
+process.ttHLeptons.tightObjects = cms.bool(True)
+process.ttHLeptons.jets = cms.InputTag("patJetsReapplyJEC")
+process.ttHLeptons.JECTag = "patJetCorrFactorsReapplyJEC"
+
+if options.dump:
+    process.dump = cms.EDAnalyzer("EventContentAnalyzer")
+    process.dpath = cms.Path(process.dump)
+    process.maxEvents.input = cms.untracked.int32(1)
+
+process.end = cms.EndPath()
+
+for channel in options.channels:
+    if channel == 'ttl':
+        taus = (2, 2)
+        leptons = (1, 1)
+        jets = (3,)
+        tags = (1, -1)
+    elif channel == 'tll':
+        taus = (0, 1)
+        leptons = (2, 2)
+        jets = (4,)
+        tags = (1, -1)
+    else:
+        raise ValueError("channel needs to be either tll or ttl")
+
+    prod = cms.EDProducer("TauProcessor",
+                          sample=cms.string(options.sample),
+                          data=cms.bool(options.data),
+                          electrons=cms.InputTag("ttHLeptons"),
+                          muons=cms.InputTag("ttHLeptons"),
+                          taus=cms.InputTag("ttHLeptons"),
+                          jets=cms.InputTag("patJetsReapplyJEC"),
+                          # jets=cms.InputTag("slimmedJets"),
+                          genJets=cms.InputTag("slimmedGenJets"),
+                          minTaus=cms.uint32(taus[0]),
+                          maxTaus=cms.uint32(taus[1]),
+                          minLeptons=cms.uint32(leptons[0]),
+                          maxLeptons=cms.uint32(leptons[1]),
+                          minJets=cms.uint32(jets[0]),
+                          minTags=cms.uint32(tags[0]),
+                          maxTags=cms.int32(tags[1]),
+                          minJetPt=cms.double(20.),
+                          minTagPt=cms.double(20.),
+                          maxJetEta=cms.double(2.4),
+                          filterPUJets=cms.bool(False),
+                          takeAll=cms.bool(options.takeAll),
+                          saveGenInfo=cms.bool(options.saveGenInfo),
+                          tauVLoose=cms.bool(options.takeVLoose),
+                          printPreselection=cms.bool(False),
+                          triggerResults=cms.string(options.hlt),
+                          debugEvents=cms.vuint32()
+                          )
+
+    selector = cms.EDFilter("TauSelector",
+                            input=cms.InputTag(channel + "Taus")
+                            )
+
+    output = cms.OutputModule(
+        "PoolOutputModule",
+        fileName=cms.untracked.string(add_channel(channel, options.outputFile)),
+        outputCommands=cms.untracked.vstring(['drop *', 'keep *_{}_*_*'.format(channel + "Taus")]),
+        SelectEvents=cms.untracked.PSet(SelectEvents=cms.vstring(channel + "Path")),
+        dataset=cms.untracked.PSet(filterName=cms.untracked.string(''))
+    )
+
+    setattr(process, channel + "Taus", prod)
+    setattr(process, channel + "Selector", selector)
+
+    setattr(process, channel + "Output", output)
+
+    path = cms.Path()
+    path *= getattr(process, channel + "Taus")
+    path *= getattr(process, channel + "Selector")
+
+    setattr(process, channel + "Path", path)
+
+    process.end *= getattr(process, channel + "Output")
+
+convertToUnscheduled(process)
